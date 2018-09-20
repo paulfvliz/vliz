5d5cc9959eef76bd0f2a0978986fd9e32939f294
Paul Focke <paul.focke@vliz.be>
2018-09-20 16:14:53 +0200

    The characters event is not the right place to start processing. Instead just collect all  the possible characters in a buffer. Once the element has ended read the buffer and process the data.

--- OpenSeaLab/src/vectorLayers/SAXHandler.java
+++ OpenSeaLab/src/vectorLayers/SAXHandler.java
@@ -27,12 +27,12 @@ public class SAXHandler extends DefaultHandler {
 
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		sb = new StringBuilder();
 		if (qName.equals("gml:featureMember") || qName.equals("wfs:member")) {
 			feature = new Feature();
 			featureCollection.addFeature(feature);
 			multi = false;
 			isFeature = true;
-			sb = new StringBuilder();
 			return;
 		}
 		element = qName.split(":")[1];
@@ -64,7 +64,6 @@ public class SAXHandler extends DefaultHandler {
 		}
 		if (polygon && (endElement.equals("exterior") || endElement.equals("interior"))) {
 			pol.addRing(GeometryFactory.createPoints(sb.toString()));
-			sb = new StringBuilder();
 			return;
 		}
 		if (endElement.equals("Polygon")) {
@@ -80,16 +79,19 @@ public class SAXHandler extends DefaultHandler {
 			}
 
 			polygon = false;
-			sb = new StringBuilder();
 			return;
 		}
 		if (endElement.equals("Point")) {
 			if (multi) {
 				// TODO deal with multipoint
 			} else {
-				feature.setGeometry(GeometryFactory.newPoint(sb.toString()));
+				String s = sb.toString();
+				if (s.contains(",")) {
+					String[] splited = s.split(",");
+					s = splited[1] + " " + splited[0];
+				}
+				feature.setGeometry(GeometryFactory.newPoint(s));
 			}
-			sb = new StringBuilder();
 			point = false;
 			return;
 		}
@@ -101,8 +103,26 @@ public class SAXHandler extends DefaultHandler {
 				feature.setGeometry(GeometryFactory.newLineString(sb.toString()));
 			}
 			lineString = false;
-			sb = new StringBuilder();
 			return;
+		}
+
+		if (endElement.equals("lowerCorner")) {
+			Point p = (Point) GeometryFactory.newPoint(sb.toString());
+			if (isFeature) {
+				feature.getBbox()[0] = p;
+			}
+			return;
+		}
+		if (endElement.equals("upperCorner")) {
+			Point p = (Point) GeometryFactory.newPoint(sb.toString());
+			if (isFeature) {
+				feature.getBbox()[1] = p;
+			}
+			return;
+		}
+
+		if (isFeature) {
+			feature.addProperty(element, sb.toString());
 		}
 
 		element = "";
@@ -113,40 +133,8 @@ public class SAXHandler extends DefaultHandler {
 		if (new String(ch, start, length).trim().length() == 0)
 			return;
 		String s = new String(ch, start, length).replace("\t", " ");
-
-		if (element.equals("lowerCorner")) {
-			Point p = (Point) GeometryFactory.newPoint(s);
-			if (isFeature) {
-				feature.getBbox()[0] = p;
-			}
-			return;
-		}
-		if (element.equals("upperCorner")) {
-			Point p = (Point) GeometryFactory.newPoint(s);
-			if (isFeature) {
-				feature.getBbox()[1] = p;
-			}
-			return;
-		}
-		if (polygon) {
-			sb.append(s);
-			return;
-		}
-		if (point) {
-			if (s.contains(",")) {
-				String[] splited = s.split(",");
-				s = splited[1] + " " + splited[0];
-			}
-			sb.append(s);
-			return;
-		}
-		if (lineString) {
-			sb.append(s);
-			return;
-		}
-		if (isFeature) {
-			feature.addProperty(element, s);
-		}
+		sb.append(s);
+		return;
 	}
 
 	public FeatureCollection getFeatures() {
